bba1498b5e7a34355781292cc2f1124b8aab436e
Lingyun Gao <glingyun@umich.edu>
2016-05-12 22:36:53 -0400

    add the example of finding the compatible states

--- example_with_sensor.py
+++ example_with_sensor.py
@@ -13,4 +13,14 @@ declaration = init(G, uncon_comp_tups, contactor_tups)
 states = {'G1': 1, 'G2': 0, 'T1': 0, 'T2': 0, 'C1': 1, 'C2': 1, 'C3': 1, 'C4': 0, 'C5': 0, 'C6': 0}
 # get the measurement
 sensor = sensor_measurement(G, uncon_comp_tups, contactor_tups, states)
-print sensor
+
+# example of finding the compatible states
+C_c = {'C1': 1, 'C2': 1, 'C3': 1}
+S_readings = {'S1': 0, 'S2': 0}
+compatible_states(G, S_readings, C_c, '0.csv')
+S_readings = {'S1': 0, 'S2': 1}
+compatible_states(G, S_readings, C_c, '1.csv')
+S_readings = {'S1': 1, 'S2': 0}
+compatible_states(G, S_readings, C_c, '2.csv')
+S_readings = {'S1': 1, 'S2': 1}
+compatible_states(G, S_readings, C_c, '3.csv')
