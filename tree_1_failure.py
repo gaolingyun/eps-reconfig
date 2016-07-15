ddd6850f7c3cd0008d2de99c53b547e3c585ab73
Lingyun Gao <glingyun@umich.edu>
2016-07-15 16:55:07 -0400

    Update tree_1_failure.py

--- tree_1_failure.py
+++ tree_1_failure.py
@@ -8,13 +8,16 @@ G = read_netlist(filename)
 uncon_comp_tups = []
 contactor_tups = []
 declaration = init(G, uncon_comp_tups, contactor_tups)
-fail_sensors = ['S1', 'S2']
+con_conts = ['C1', 'C3', 'C4', 'C6']
+fail_sensors = ['S1']
+known_sensors = sensors_not_connected_with_generators(G, con_conts)
 
 '''
 sensors = ['S1', 'S2', 'S3']
-con_conts = ['C1', 'C3', 'C4', 'C6']
 result = generate_database(G, sensors, con_conts, read_file_name)
 '''
+
+# eliminate faulty sensors
 
 actual_state = {'G2': 1, 'G1': 0, 'T2': 0, 'T1': 0, 'C2': 0, 'C5': 1}
 
@@ -23,8 +26,24 @@ for j in range(0, pow(2, len(actual_state))):
 	state_value = format(j, '0' + str(len(actual_state)) + 'b')
 	for i in range(0, len(actual_state)):
 		actual_state[list(actual_state)[i]] = int(state_value[i])
+	'''
 	print 'actual: '
 	print actual_state
+	'''
+
+	
+	
+	'''
+	if j == 0:
+		action = {'C1': 0, 'C3': 0, 'C4': 0, 'C6': 0}
+		states = actual_state.copy()
+		states.update(action)
+		sensor_readings = sensor_measurement(G, uncon_comp_tups, contactor_tups, states)
+		assign_false_values(sensor_readings, []) # readings given by the circuit
+		for i in known_sensors:
+			if sensor_readings[i] == 1:
+				fail_sensors.remove(i)
+	'''
 
 	# set up the initial action
 	action = {'C1': 1, 'C3': 1, 'C4': 1, 'C6': 1}
@@ -38,9 +57,10 @@ for j in range(0, pow(2, len(actual_state))):
 		compatible_states.append([])
 
 	count = 0
+	timetaken = 0
 	while (len(compatible_states) >= 0):
 		sensor_readings = sensor_measurement(G, uncon_comp_tups, contactor_tups, states)
-		assign_false_values(sensor_readings, ['S1']) # readings given by the circuit
+		assign_false_values(sensor_readings, []) # readings given by the circuit
 
 		# consider all the cases when some sensors are faulty
 		compatible_states_candidate = []
@@ -69,15 +89,27 @@ for j in range(0, pow(2, len(actual_state))):
 						temp_states[i].append(k.copy())
 			compatible_states = list(temp_states)
 
-		if len(action_list) == 6: break
+		if len(action_list) == 16: break
+		#begin = time.time()
 		action = build_tree(G, read_file_name, compatible_states, action_list, fail_sensors)
-		if action == {}: break
+		#end = time.time()
+		#timetaken += end - begin
+		#if action == {}: break
 		states.update(action)
 		action_list.append(action.copy())
-		print action
 		count += 1
-
+	#print timetaken/5.0
+	
+	
+	length = 0
+	
+	for i in compatible_states:
+		length += len(i)
+	print length
+	'''
 	for i in compatible_states:
 		print i
 	print '---'
-	if j == 2: break
+	'''
+	
+	#if j == 1: break
